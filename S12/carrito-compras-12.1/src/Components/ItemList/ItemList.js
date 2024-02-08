595c35882b7fb1490665cae5555a6902a29e98c7
Rodri <rodrigoalvesdemacedo@live.com>
2024-02-08 20:48:05 +0100

    Update tarea 12.1 - Carrita de compra

--- S12/carrito-compras-12.1/src/Components/ItemList/ItemList.js
+++ S12/carrito-compras-12.1/src/Components/ItemList/ItemList.js
@@ -20,7 +20,7 @@ const reducer = (state, action) => { // Aquí es donde ocurre la transformación
     switch (action.type) {
 
         case "ADD ITEM":
-            console.log("hoy soy la primera acction")
+            console.log("Soy la accion ADD ITEM")
             const newItem = {
                 id: state.lastItemCreated + 1,
                 name: action.payload.name,
@@ -33,6 +33,8 @@ const reducer = (state, action) => { // Aquí es donde ocurre la transformación
             break;
 
         case "DELETE ITEM":
+            console.log("Me estoy ejecutando, soy el remover")
+            newState.items = newState.items.splice(action.payload.id)
             break;
         default:
             console.log("ACTION NOT SUPPORTED");
@@ -45,6 +47,11 @@ const reducer = (state, action) => { // Aquí es donde ocurre la transformación
 const ItemList = () => {
 
     const [state, dispatch] = React.useReducer(reducer, initialValue)
+
+    // Función para calcular el total de todos los totales por acumulación
+    const calculateTotal = () => {
+        return state.items.reduce((acum, item) => acum + parseFloat(item.total), 0);
+    };
 
     const inputNameReference = React.useRef(null);
     const inputPriceReference = React.useRef(null);
@@ -66,41 +73,62 @@ const ItemList = () => {
         console.log(payloadToSend);
     }, [])
 
+    const removeItem = React.useCallback((itemId) => {
+
+        const payloadToRemove = {
+            id: itemId,
+        }
+
+        dispatch({ type: "DELETE ITEM", payload: payloadToRemove });
+
+    }, [])
+
+
+
 
     return (
         <div className="cart-container">
-            <h2 className="cart-title">Cart 🛒</h2>
-            <form className="add-item-form" onSubmit={onSubmit}>
-                <label className="form-label">
-                    <p className="form-label-text">What do you need to purchase? 🧺</p>
-                    <input className="form-input" ref={inputNameReference} name="product" type="text" placeholder="E.g.: Bread" />
-                </label>
-                <label className="form-label">
-                    <p className="form-label-text">How much does it cost? 💸 </p>
-                    <input className="form-input" ref={inputPriceReference} name="price" type="text" placeholder="E.g.: 5.50" />
-                </label>
-                <label className="form-label">
-                    <p className="form-label-text">How many do you need? 🔢</p>
-                    <input className="form-input" ref={inputUnitsReference} name="units" type="number" />
-                </label>
-                <button className="submit-button" type="submit">Add it to the list 👇🏽!</button>
-            </form>
+            <div>
+                <h2 className="cart-title">Cart 🛒</h2>
 
-            <h3 className="item-list-title">Your choices</h3>
-            <div className="item-list-container">
-                {state.items?.map((item) => {
-                    return (
-                        item.id > 0 ? (
-                            <div className="item-card" key={item.id}>
-                                <p className="item-name">Product: {item.name}</p>
-                                <p className="item-price">Price: {item.price}€</p>
-                                <p className="item-units">Unit: {item.units}</p>
-                                <p className="item-total">Total: {item.total}€</p>
-                                <button className="item-card-button">Remove item</button>
-                            </div>
-                        ) : null
-                    )
-                })}
+                <form className="add-item-form" onSubmit={onSubmit}>
+                    <label className="form-label">
+                        <p className="form-label-text">What do you need to purchase? 🧺</p>
+                        <input className="form-input" ref={inputNameReference} name="product" type="text" placeholder="E.g.: Bread" />
+                    </label>
+                    <label className="form-label">
+                        <p className="form-label-text">How much does it cost? 💸 </p>
+                        <input className="form-input" ref={inputPriceReference} name="price" type="text" placeholder="E.g.: 5.50" />
+                    </label>
+                    <label className="form-label">
+                        <p className="form-label-text">How many do you need? 🔢</p>
+                        <input className="form-input" ref={inputUnitsReference} name="units" type="number" />
+                    </label>
+                    <button className="submit-button" type="submit">Add it to the list 👇🏽</button>
+                </form>
+            </div>
+            <div>
+                <h2 className="item-list-title">Your choices</h2>
+                <div className="item-list-container">
+                    {state.items?.map((item) => {
+                        return (
+                            item.id > 0 ? (
+                                <div className="item-card" key={item.id}>
+                                    <div className="item-card-info">
+                                        <p className="item-name">Product: {item.name}</p>
+                                        <p className="item-price">Price: {item.price}€</p>
+                                        <p className="item-units">Unit: {item.units}</p>
+                                        <p className="item-total">Total: {item.total}€</p>
+                                    </div>
+                                    <div>
+                                        <button className="item-card-button" onClick={() => removeItem(item.id)}>X</button>
+                                    </div>
+                                </div>
+                            ) : (<p>Your list is currently empty</p>)
+                        )
+                    })}
+                    {calculateTotal() !== 0 ? <h3>TOTAL: {calculateTotal()}€</h3>: null}
+                </div>
             </div>
         </div>
     )
