738885e24f1ea92d58ea86f8e84a08e7e87ed292
Rodri <rodrigoalvesdemacedo@live.com>
2024-02-22 00:38:14 +0100

    Tarea 14.1 hecha

--- S14/proyecto-14.1/src/App.js
+++ S14/proyecto-14.1/src/App.js
@@ -1,11 +1,41 @@
-import "./App.scss";
-import RegisterForm from "./Components/RegisterForm/RegisterForm";
+import React from 'react';
+import './App.scss';
+import RegisterForm from './Components/RegisterForm/RegisterForm';
+import Spanish from '../src/Lang/es.json';
+import French from '../src/Lang/fr.json';
+import English from '../src/Lang/en.json';
+import { IntlProvider } from 'react-intl';
+
+const locale = navigator.language;
+let defaultMessages = Spanish;
+switch (locale) {
+  case 'fr-FR':
+    defaultMessages  = French;
+    break;
+  case 'en-EN':
+    defaultMessages  = English;
+    break;
+  case 'es-ES':
+    defaultMessages  = Spanish;
+    break;
+  default:
+    defaultMessages  = English;
+}
 
 function App() {
+  const [messages, setMessages] = React.useState(defaultMessages);
+
   return (
-    <div className='App'>
-      <RegisterForm/>
-    </div>
+    <IntlProvider locale={locale} messages={messages}>
+      <div className='App'>
+        <RegisterForm />
+      </div>
+      
+      <h2>Language selector</h2>
+      <button onClick={() => setMessages(Spanish)}>Spanish</button>
+      <button onClick={() => setMessages(English)}>English</button>
+      <button onClick={() => setMessages(French)}>French</button>
+    </IntlProvider>
   );
 }
 
--- S14/proyecto-14.1/src/Components/RegisterForm/RegisterForm.js
+++ S14/proyecto-14.1/src/Components/RegisterForm/RegisterForm.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import './RegisterForm.scss';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 const RegisterForm = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
   const [formData, setFormData] = React.useState(null);
 
   const onSubmit = (data) => setFormData(data);
+  const { formatMessage } = useIntl();
+
+  console.log(formatMessage({id: 'hello'}));
 
   return (
     <div className='register-form'>
       <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset className='register-form__fieldset'>
           <input
-            placeholder='Type your name here...'
+            placeholder={formatMessage({ id:'register_form:first_name' })}
             type='text'
             {...register('firstName', { required: true })}
           />
-          { errors?.firstName && <p className='register-form__error'>Este campo es obligatorio</p>}
+          {errors?.firstName && (
+            <p className='register-form__error'><FormattedMessage id='register_form:required_field' /></p>
+          )}
+          
         </fieldset>
         <fieldset className='register-form__fieldset'>
           <input
-            placeholder='Now your last name...'
+            placeholder={formatMessage({ id:'register_form:last_name' })}
             type='text'
             {...register('lastName', { required: true, minLength: 3 })}
           />
-          { errors?.lastName && <p className='register-form__error'>Este campo es obligatorio y debe tener al menos 3 caracteres</p>}
+          {errors?.lastName && (
+            <p className='register-form__error'>
+              <FormattedMessage id='register_form:required_field_3_chars' />
+            </p>
+          )}
+          
         </fieldset>
 
         <button type='submit'>Aceptar</button>
